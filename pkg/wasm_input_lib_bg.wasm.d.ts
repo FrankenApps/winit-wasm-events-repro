a6f000c09a59091cde56cc1cec3acb7ebec1a999
FrankenApps <FrankenApps@users.noreply.github.com>
2021-10-19 11:39:00 +0000

    deploy: 1d23a08aa5edbe95131f5742865d03e5b5d3e442

---
+++ pkg/wasm_input_lib_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function open_window(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2b43287c77d281f3(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hadb7c704a9e677ef(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
